5ef61e461b5c0725b33222d35c70e60e61a53e32
Saifeddin Matoui <saifeddinmatoui4@gmail.com>
2018-04-23 10:04:28 +0100

    add models

--- coretabs/views.py
+++ coretabs/views.py
@@ -3,6 +3,6 @@ from django.http import HttpResponse
 def index(request):
     return HttpResponse("Hi there !!")
 
-def hello(request):
-    query = request.GET.get('name')
-    return  HttpResponse("Hi {}".format(query))
+def hello(request, name):
+    
+    return  HttpResponse("Hi {}".format(name))

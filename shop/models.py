733fbda9195aa13cfb490c74aa00f83b0f07c55a
Saifeddin Matoui <saifeddinmatoui4@gmail.com>
2018-04-24 10:01:07 +0100

    add category to models

--- shop/models.py
+++ shop/models.py
@@ -7,16 +7,16 @@ class Product(models.Model):
 	created_at = models.DateTimeField(auto_now_add=True)
 	description = models.TextField(blank=True)
 	slug = models.SlugField(unique=True, blank=True)
-	category = models.ForeignKey(Category, related_name='products', on_delete=models.CASCADE)
+	category = models.ForeignKey('Category', related_name='products', on_delete=models.CASCADE)
 
 
-class Meta():
-	ordering = ('name', )
+	class Meta():
+		ordering = ('name', )
 
 class Category(models.Model):
 	name = models.CharField(max_length=100)
 	slug = models.SlugField(unique=True, blank=True)
 	created_at = models.DateTimeField(auto_now_add=True)
 
-class Meta():
-	ordering = ('name', )
+	class Meta():
+		ordering = ('name', )

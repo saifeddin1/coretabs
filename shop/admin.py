a7ba09a1b7a56368e8c168574d751f584bf35456
Saifeddin Matoui <saifeddinmatoui4@gmail.com>
2018-05-02 13:14:23 +0100

    fix discount func

--- shop/admin.py
+++ shop/admin.py
@@ -1,27 +1,28 @@
 from django.contrib import admin
 from . import models
-
-
-def make_price_zero(modeladmin, request, queryset):
-    queryset.update(price=0)
-
-make_price_zero.short_description = "Make selected products free"
-
-def make_discount(modeladmin, request, queryset):
-    queryset.update(price=-20)
-    
-
-make_discount.short_description = "Make a 20 percent discount on selected items"
+import decimal
 
 admin.site.site_header = "Coretabs Online Shop Administration"
 admin.site.index_title = ""
+
+def make_price_zero(modeladmin, request, queryset):
+    queryset.update(price=0)
+make_price_zero.short_description = "Make selected products free"
+
+def discount(self, request, queryset):
+    for product in queryset:
+        product.price = product.price * decimal.Decimal('0.8')
+        product.save()
+discount.short_description = 'Apply 20%% DISCOUNT'
+    
+
 @admin.register(models.Product)
 class ProductAdmin(admin.ModelAdmin):
    date_hierarchy = 'created_at'
    search_fields = ['name']
    list_display = ('name', 'price', 'stock', 'category',)
    list_filter = ('created_at', 'category',)
-   actions = [make_price_zero, make_discount]
+   actions = [make_price_zero, discount]
 
  
 @admin.register(models.Category) # This will add Category to admin 
